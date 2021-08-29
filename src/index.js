b6da695ef9c24428bd0dc08318df1ed620e13c8b
Bety Martinez <trism460@gmail.com>
2021-08-28 23:10:28 -0500

    Add diferent replies

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const token = process.env.TOKEN;
-const chanelID = process.env.CHANEL_ID;
+const channelID = process.env.CHANNEL_ID;
+const replies = ['Pika-pika', 'Pika', 'Pika-Pikachu', 'Chuuu', 'Pikachuuuu'];
 
 client.login(token);
 
@@ -13,7 +14,9 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', message => {
-    if (message.channel.id == chanelID && message.content == 'pikachu') {
-        message.reply('Pika-pika');
+    if (message.channel.id == channelID && message.content == 'Pikachu') {
+        // message.reply('Pika-pika');
+        const index = Math.floor(Math.random() * replies.length);
+        message.channel.send(replies[index]);
     }
 });
