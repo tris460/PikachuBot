ba4fc9059b8d329378fcabe3000f8b7d691057be
Bety Martinez <trism460@gmail.com>
2021-08-28 22:07:29 -0500

    Add env variables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 console.log('Beep beep!');
 
+require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const token = process.env.TOKEN;
 
 client.login(token);
 client.on('ready', () => {
