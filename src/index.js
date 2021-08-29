726db137873c6cc1b3ce37826e3421f7aec92ec0
Bety Martinez <trism460@gmail.com>
2021-08-28 22:51:31 -0500

    Add a channel validation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const token = process.env.TOKEN;
+const chanelID = process.env.CHANEL_ID;
 
 client.login(token);
 
@@ -12,7 +13,7 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', message => {
-    if (message.content == 'pikachu') {
+    if (message.channel.id == chanelID && message.content == 'pikachu') {
         message.reply('Pika-pika');
     }
 });
